aaf02a589e74cf9053a4956386313007e0ec65c1
Josh <jracz419@gmail.com>
2024-03-27 15:56:38 -0400

    fixed javascript tag

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ function myFunction() {
     let input, filter, ul, li, a, i, txtValue;
     input = document.getElementById('myInput');
     filter = input.value.toUpperCase();
-    ul = document.getElementById("myULdir");
+    ul = document.getElementById("myUL");
     li = ul.getElementsByTagName('li');
   
     // Loop through all list items, and hide those who don't match the search query
